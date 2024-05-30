423369dd9baf5fcc97da4906acc8b9e117aaefc1
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-05-30 19:05:33 +0330

    Customize Admin Panel in 2 ways

--- Hajihosseini/Weblog/blog/admin.py
+++ Hajihosseini/Weblog/blog/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import BlogPost
 
 
-admin.site.register(BlogPost)
+# مدل دوم با استفاده از دکوریتور
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'status', 'author', 'likes', 'datetime_modified']
+    list_display_links = ['author', 'likes', 'datetime_modified']
+    list_editable = ['status']
+    ordering = ['-status', '-likes', 'id']
+
+
+# مدل اول 
+# admin.site.register(BlogPost, BlogPostAdmin)
