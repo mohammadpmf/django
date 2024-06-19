56e20d72e4332224bb0ae0c39d6bc2b4c2d8e032
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-19 16:45:01 +0330

    Add Create Post

--- Hajihosseini/Weblog/blog/views.py
+++ Hajihosseini/Weblog/blog/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from .models import BlogPost
+from django.contrib.auth import get_user_model
 
 
 def post_list_view(request):
-    posts = BlogPost.objects.filter(status='p')
+    posts = BlogPost.objects.filter(status='p').order_by('-datetime_created')
     context = {
         'posts': posts,
     }
@@ -19,4 +20,16 @@ def post_detail_view(request, pk):
     return render(request, 'blog/post_detail.html', context=context)
 
 def post_create_view(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        BlogPost.objects.create(
+            title = title,
+            text = text,
+            author = get_user_model().objects.last(),
+            status = 'p',
+        )
+        return redirect('posts_list')
+    else:
+        print('get')
     return render(request, 'blog/post_create.html')
