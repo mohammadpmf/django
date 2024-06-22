b4145cedf2ca7ba9cb953195786471b0e1a8f54f
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-22 15:38:45 +0330

    change forms

--- Hajihosseini/Bookstore/accounts/forms.py
+++ Hajihosseini/Bookstore/accounts/forms.py
@@ -6,9 +6,11 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('nat_code', 'gender')
+        # fields = UserCreationForm.Meta.fields + ('nat_code', 'gender')
+        fields = ['first_name', 'last_name', 'email', 'nat_code', 'gender', 'username']
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        # fields = UserChangeForm.Meta.fields
+        fields = ['username']
