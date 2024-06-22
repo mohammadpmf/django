0b34a5e2426b23ec181fb2532810c374cd02df15
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-22 17:56:15 +0330

    Add crispy forms to project and connect html files to each other

--- Hajihosseini/Bookstore/config/settings.py
+++ Hajihosseini/Bookstore/config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #
+    'crispy_forms',
+    'crispy_bootstrap5',
+
     # my apps
     'accounts',
 ]
@@ -128,3 +132,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+
+
+
+########################     CRISPY FORMS     ########################
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+########################     END CRISPY FORMS     ########################
