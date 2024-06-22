fd0dbcc5ba50b1352d1039942e99d28cc6b236ca
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-22 14:42:21 +0330

    Remove extra codes from Hajihosseini's code but don't delete forms.py

--- Hajihosseini/Bookstore/accounts/admin.py
+++ Hajihosseini/Bookstore/accounts/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
-from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+    add_form = UserCreationForm
+    form = UserChangeForm
     list_display = UserAdmin.list_display[:4] + ('nat_code', 'gender') + UserAdmin.list_display[4:]
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('nat_code', 'gender')}),
