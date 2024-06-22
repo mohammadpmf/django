9dcdb8ad50d56e04b1ee769af053177ff1bf8149
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-22 15:30:35 +0330

    Implement Login & Signup

--- Hajihosseini/Bookstore/accounts/views.py
+++ Hajihosseini/Bookstore/accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+
+
+class SignUpView(generic.CreateView):
+    template_name = 'registration/signup.html'
+    model = CustomUser
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
--- Hajihosseini/Bookstore/config/settings.py
+++ Hajihosseini/Bookstore/config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,3 +126,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
