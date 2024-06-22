ac93e90149f434672f7337811dd898610f9b4d7a
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-22 13:31:42 +0330

    Add gender and nat_code to customuser

--- Hajihosseini/Bookstore/accounts/models.py
+++ Hajihosseini/Bookstore/accounts/models.py
@@ -3,4 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    pass
+    GENDER_FEMALE = 'f'
+    GENDER_MALE = 'm'
+    GENDER_CHOICES =(
+        (GENDER_FEMALE, 'Female'),
+        (GENDER_MALE, 'Male'),
+    )
+    nat_code = models.CharField(max_length=10, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    
