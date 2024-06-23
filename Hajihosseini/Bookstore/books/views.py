ed3acc83342f3414a1ea5d0473bb595666fc486a
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-23 19:59:20 +0330

    Add pagination

--- Hajihosseini/Bookstore/books/views.py
+++ Hajihosseini/Bookstore/books/views.py
@@ -9,6 +9,7 @@ class BookListView(generic.ListView):
     model = Book
     template_name = 'books/book_list.html'
     context_object_name = 'books'
+    paginate_by=4
 
 
 class BookDetailView(generic.DetailView):
