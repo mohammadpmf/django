5ce672e275c12532b15418ea5719ca224345f460
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-24 17:59:02 +0330

    Use forms for books to write better code and tidy codes up

--- Hajihosseini/Bookstore/books/forms.py
+++ Hajihosseini/Bookstore/books/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 
-from .models import Comment
+from .models import Book, Comment
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text', 'is_recommended', )
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('title', 'author', 'description', 'price', 'cover', 'translator', 'publisher', 'year_published', 'number_of_pages', )
--- Hajihosseini/Bookstore/books/views.py
+++ Hajihosseini/Bookstore/books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 
 from .models import Book, Comment
-from .forms import CommentForm
+from .forms import BookForm, CommentForm
 
 
 class BookListView(generic.ListView):
@@ -21,13 +21,13 @@ class BookDetailView(generic.DetailView):
     template_name = 'books/book_detail.html'
     context_object_name = 'book'
     
-    def get_context_data(self,*args, **kwargs):
-        book=kwargs.get('object')
-        comment_form = CommentForm()
+    def get_context_data(self,*args, **kwargs): # اگه بخوایم تابع get_context_data رو بنویسیم، kwargs رو نمیشه حذف کرد. خودش کلید آبجکت رو به صورت kwargs میفرسته.
+        # book=kwargs.get('object') # اول ازش این شکلی استفاده کرده بودم. بعد تابع get_object رو یاد گرفتم که به نظرم خیلی قشنگ تر و تمیز تره
+        book = self.get_object()
         context={
             'book': book,
             'comments': Comment.objects.filter(is_approved=True, book=book.pk).order_by('-datetime_modified'),
-            'comment_form': comment_form,
+            'comment_form': CommentForm(),
             }
         return context
     
@@ -38,42 +38,32 @@ class BookDetailView(generic.DetailView):
     # کتاب رو هم ببینی. مال من میشه دید. اما نمیشه کامنت گذاشت و باید لاگین کرد. حالا اگه
     # کسی یه جوری یه متد پست بفرسته که بدون لاگین بخواد کامنت بذاره، بهش ارور میده.
     def post(self, request, *args, **kwargs):
-        pk=kwargs.get('pk')
-        text = request.POST.get('text')
-        is_recommended = request.POST.get('is_recommended')
-        if is_recommended == 'on':
-            is_recommended = True
-        else:
-            is_recommended = False
-        # توضیح این که وقتی تیک رو میزدیم، مقدار رو میفرستاد و داخلش مینوشت on که یعنی
-        # تیکش زده است و روشنه. اما وقتی تیکش رو برمیداشتیم کلا چیزی به اون اسم نمیفرستاد
-        # و مقدارش None بود که مشکل ایجاد میکرد. خلاصه در هر دو حالت ارور میداد و نمیتونست
-        # تو دیتابیس ذخیره کنه. خودم با ایف و الس بالا گفتم اگر آن بود مقدارش رو بذاره ترو
-        # اگر هم on نبود که یعنی هیچی نفرستاده و None هست. اما نمیتونیم برای ساخت یک کامنت
-        # مقدار این متغیر رو نان بذاریم. عوضش کردم به False
-        book = get_object_or_404(Book, pk=pk)
-        error = ""
+        book = self.get_object()
+        comments = Comment.objects.filter(is_approved=True, book=book).order_by('-datetime_modified')
+        comment_form = CommentForm(request.POST)
         message = None
-        if text!="":
-            Comment.objects.create(text=text, is_recommended=is_recommended, book=book, user=request.user)
-            message = f"نظر {text} از طرف {request.user} برای کتاب {book} با موفقیت ارسال شد. پس از تایید مدیریت در سایت نمایش داده خواهد شد.از نظر ارزشمند شما سپاسگزاریم."
-        else:
-            error = "متن نمیتواند خالی باشد."
+        if comment_form.is_valid():
+            comment_form = comment_form.save(commit=False)
+            comment_form.book = book
+            comment_form.user = request.user
+            comment_form.save()
+            message = f"نظر {request.POST.get('text')} از طرف {request.user} برای کتاب {book} با موفقیت ارسال شد. پس از تایید مدیریت در سایت نمایش داده خواهد شد.از نظر ارزشمند شما سپاسگزاریم."
+            comment_form = CommentForm()
         context={
             'book': book,
-            'comments': Comment.objects.filter(is_approved=True, book=book).order_by('-datetime_modified'),
-            'error': error,
-            'comment_form': CommentForm(),
+            'comments': comments,
+            'comment_form': comment_form,
             'message': message,
             }
-        return render(request, 'books/book_detail.html', context=context)
+        return render(request, 'books/book_detail.html', context)
 
 
 def book_detail_view(request, pk):
-    message = None
     book = get_object_or_404(Book, pk=pk)
     # comments = Comment.objects.filter(is_approved=True, book=book.pk).order_by('-datetime_modified')
     comments = book.comments.filter(is_approved=True, book=book.pk).order_by('-datetime_modified')
+    comment_form = CommentForm()
+    message = None
     if request.method=='POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -81,34 +71,63 @@ def book_detail_view(request, pk):
             comment_form.book = book
             comment_form.user = request.user
             comment_form.save()
-            comment_form = CommentForm()
             message = f"نظر {request.POST.get('text')} از طرف {request.user} برای کتاب {book} با موفقیت ارسال شد. پس از تایید مدیریت در سایت نمایش داده خواهد شد.از نظر ارزشمند شما سپاسگزاریم."
-    else:
-        comment_form = CommentForm()
+            comment_form = CommentForm()
     context = {
         'book': book,
         'comments': comments,
         'comment_form': comment_form,
         'message': message,
-    }
+        }
     return render(request, 'books/book_detail.html', context)
 
 
 class BookCreateView(LoginRequiredMixin, generic.CreateView):
     model = Book
+    form_class = BookForm
     template_name = 'books/book_form.html'
-    fields = ['title', 'author', 'description', 'price', 'cover', 'translator', 'publisher', 'year_published', 'number_of_pages', 'user']
+    
+    def post(self, request):
+        book_form = BookForm(request.POST, request.FILES)
+        if book_form.is_valid():
+            book_form = book_form.save(commit=False)
+            book_form.user = self.request.user
+            book_form.save()
+            return redirect(book_form.get_absolute_url())
+        context = {'form': book_form}
+        return render(request, 'books/book_form.html', context)
 
 
 class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Book
-    fields = ['title', 'author', 'description', 'price', 'cover', 'translator', 'publisher', 'year_published', 'number_of_pages', 'user']
+    form_class = BookForm
     template_name = 'books/book_update_form.html'
     context_object_name = 'book'
 
     def test_func(self):
         object = self.get_object()
         return object.user==self.request.user
+    
+    def post(self, request, *args, **kwargs): # اینجا حتما باید kwargs باشه. خودش پرایمری کی رو میاد میفرسته. وقتی نمیذاشتم ارور میداد که ارسال شده اما این تابع پست انتظارش رو نداشته. پس میذاریم که انتظارش رو داشته باشه :)
+        book:Book = self.get_object()
+        book_form = BookForm(request.POST, request.FILES, instance=book)
+        if book_form.is_valid():
+            book_form = book_form.save()
+            return redirect(book.get_absolute_url())
+        # اول این رو این مدلی که پایین نوشتم نوشته بودم که کار هم میکرد.
+        # اما داشتم تست میکردم دیدم که ایف مدل بالایی
+        # هم جواب میده. از نظر منطقی درسته. چون بهش گفتم این بوک فورمه، اینستنسش همون بوک هست.
+        # پس قاعدتا اطلاعات رو داره و هر چیزی رو که رکوئست دات پست براش ارسال کرده جایگزین میکنه
+        # اما چون یوزر رو ارسال نکرده، خودش همون یوزر قبلی رو میذاره. به هر حال کد پایینی جامع تر
+        # و طولانی تر هست. اما پاکش نکردم و کامنت کردم و گذاشتم بمونه که یادم نره و اگه تو حالت
+        # خاص مشکلی پیش اومد، سه ساعت وقتم گرفته نشه و کد آماده شو داشته باشم.
+        # if book_form.is_valid():
+        #     book_form = book_form.save(commit=False)
+        #     book_form.user = book.user
+        #     book_form.save()
+        #     return redirect(book.get_absolute_url())
+        context = {'form': book_form, 'book': book}
+        return render(request, 'books/book_update_form.html', context)
 
 
 
