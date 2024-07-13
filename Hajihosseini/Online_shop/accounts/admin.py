5a856d2193b156e4667053eca8c8fba4a4a5f544
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-07-13 17:23:05 +0330

    Add CustomUser model

--- Hajihosseini/Online_shop/accounts/admin.py
+++ Hajihosseini/Online_shop/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['email', 'username']
