e520c9c8edafa4d38d62fabb3a6d9d23722cfa5e
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-07-14 16:40:54 +0330

    Add ListView and DetailView

--- Hajihosseini/Online_shop/products/admin.py
+++ Hajihosseini/Online_shop/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ['title', 'description', 'price', 'active']
+    list_display_links = ['title', 'description', 'price']
+    list_editable = ['active']
