ab9ad4338c69e8cee9303abe9b3eb430a1e2b0bd
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-07-18 18:10:09 +0330

    Add short description to products and use verbose name for all fields and translate them

--- Hajihosseini/Online_shop/products/models.py
+++ Hajihosseini/Online_shop/products/models.py
@@ -8,14 +8,15 @@ from django_prose_editor.fields import ProseEditorField
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    # description = models.TextField()
-    description = ProseEditorField()
-    price = models.PositiveIntegerField(default=0)
-    active = models.BooleanField(default=True)
-    image = models.ImageField(verbose_name=_("Product Image"), upload_to='product/product_cover', blank=True)
-    datetime_created = models.DateTimeField(verbose_name=_("Date Time of Creation"), default=timezone.now)
-    datetime_modified = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100, verbose_name=_('Title'))
+    # description = models.TextField(verbose_name=_('Description'))
+    description = ProseEditorField(verbose_name=_('Description'))
+    short_description = models.TextField(max_length=300, blank=True, verbose_name=_('Short description')) # حاجی حسینی این مکس لنگث رو برداشته بود و تو اچ تی ام ال فیلتر زده بود. اما من گذاشتم خودش تو تکست فیلد پنل ادمین اجازه تایپ بیشتر از ۳۰۰ کاراکتر رو نمیده. خیلی خفنه. اما فیلترش رو هم تو اچ تی ام ال گذاشتم که باشه.
+    price = models.PositiveIntegerField(default=0, verbose_name=_('Price'))
+    active = models.BooleanField(default=True, verbose_name=_('Active State'))
+    image = models.ImageField(upload_to='product/product_cover', blank=True, verbose_name=_("Product Image"))
+    datetime_created = models.DateTimeField(default=timezone.now, verbose_name=_("Date Time of Creation"))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_("Date Time of last edit"))
 
     def __str__(self):
         return self.title
@@ -32,13 +33,13 @@ class Comment(models.Model):
         ('4', _('Good')),
         ('5', _('Perfect')),
     )
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', verbose_name=_("Product"))
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments', verbose_name=_("Author"))
     body = models.TextField(verbose_name=_('Comment Body'))
     stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('Score to this product'))
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
+    datetime_created = models.DateTimeField(auto_now_add=True, verbose_name=_("Date Time of Creation"))
+    datetime_modified = models.DateTimeField(auto_now=True, verbose_name=_("Date Time of last edit"))
+    active = models.BooleanField(default=True, verbose_name=_("Active State"))
 
     def __str__(self):
         return self.body
